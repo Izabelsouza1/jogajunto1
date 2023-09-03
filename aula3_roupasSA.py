7e84bbeef8642941e5bdd01c3d06a8be1dbe7bd0
Izabelsouza1 <izafigueiroa@hotmail.com>
2023-09-03 22:19:39 +0100

    aula 3 roupas SA

---
+++ aula3_roupasSA.py
@@ -0,0 +1,19 @@
+
+nome = "PAULA"
+sobrenome = "MARTINS"
+mes = "JANEIRO"
+valor = "500,00"
+desconto = "10"
+cupom = nome + "É" + desconto
+
+
+print("Olá, " + nome + sobrenome +". Em "+ mes + " você realizou uma compra no valor de R$" + valor + " e ganhou um desconto de " + desconto + "% em sua próxima compra. Use o cupom " + cupom+".")
+
+
+
+
+
+
+
+#Olá, PAULA MARTINS. Em JANEIRO você realizou uma compra no valor de R$500,00
+ #e ganhou um desconto de 10% em sua próxima compra. Use o cupom PAULAÉ10.
