f4faa0dd92ed74cedd22ee7d0a0d914a3ad6fa92
Izabelsouza1 <izafigueiroa@hotmail.com>
2023-09-08 13:24:12 +0100

    clear

---
+++ aula4.py
@@ -0,0 +1,15 @@
+#Agora, implemente uma nova feature: a funcionalidade de notas.
+
+##Para isso, insira duas variáveis com espaço para o input e uma terceira com o valor somado da operação. 
+##Lembre-se que o tipo de dado retornado da função input, é sempre uma string.
+##Ao encerrar, faça o push para seu repositório do github e compartilhe o link com a pessoa facilitadora.
+
+print("Olá, aluno para saber as somas das suas notas, digite nota 1 e nota 2")
+
+nome = input("digite seu nome:")
+nota1 = input("digite primeira nota:")
+nota2 = input("digite sua segunda nota:")
+soma_2_notas = int(nota1) + int(nota2)
+
+
+print(f" {nome} a soma de suas notas é {soma_2_notas}")
