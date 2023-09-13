484c8f01f669d381609181a831841df6bfab46d3
Izabelsouza1 <izafigueiroa@hotmail.com>
2023-09-13 18:45:14 +0100

    aula 6Atv1

---
+++ aula6atv1.py
@@ -0,0 +1,14 @@
+
+idade = int(input("digite sua idade:"))
+
+if idade > 18:
+    print("Indivíduo possui idade mínima para dirigir")
+
+elif idade == 18 or idade <= 17:
+    print("Indivíduo tem entre 17 e 18 anos e ainda NÃO está apto para dirigir")
+
+else:
+    print("Individuio não possui idade minima para  dirigir")
+    
+
+    
